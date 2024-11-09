6d6656b716dbd6ff5f0cb4a6593d0f69dd82932e
diatribes <diatribes@gmail.com>
2024-11-09 09:18:49 -0500

    timerfd in kernel to support sdhcp

--- rootfs.sh
+++ rootfs.sh
@@ -1,6 +1,14 @@
 #!/bin/bash 
 CACHEDIR=cache
 cd "${CACHEDIR}"
+
+git clone git://git.2f30.org/sdhcp
+cd sdhcp
+make CC=musl-gcc LDFLAGS=--static
+mkdir -vp ../../src/overlay/usr/bin
+cp -v sdhcp ../../src/overlay/usr/bin/
+cd ..
+
 if [ ! -d "toybox" ]; then
     git clone https://github.com/landley/toybox
     cd toybox
@@ -9,11 +17,11 @@ if [ ! -d "toybox" ]; then
 else
     cd toybox
 fi
-cp ../../src/qemu-toybox-config .config
+cp -v ../../src/qemu-toybox-config .config
 LINUX="${CACHEDIR}/${KERNELVER}" LDFLAGS=--static CROSS_COMPILE=x86_64-linux-musl-cross/bin/x86_64-linux-musl- make root
-cp ../../src/dumb-init/dumb-init ../../src/overlay
-cp ../../src/carl-exit/carl-exit ../../src/overlay
-cp -r ../../src/overlay/* root/x86_64/fs/
+cp -v ../../src/dumb-init/dumb-init ../../src/overlay
+cp -v ../../src/carl-exit/carl-exit ../../src/overlay
+cp -vr ../../src/overlay/* root/x86_64/fs/
 chmod +x root/x86_64/fs/dumb-init
 chmod +x root/x86_64/fs/carl-exit
 chmod +x root/x86_64/fs/init
